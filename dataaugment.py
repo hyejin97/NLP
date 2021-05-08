4065952b30b774456765a58666ee44e29b76d001
Hyejin Kim <42766656+hyejin97@users.noreply.github.com>
2021-05-08 02:29:45 -0500

    Add files via upload
    add dataaugmentation

---
+++ dataaugment.py
@@ -0,0 +1,13 @@
+import nlpaug.augmenter.char as nac
+import nlpaug.augmenter.word as naw
+import nlpaug.augmenter.sentence as nas
+import nlpaug.flow as nafc
+
+from nlpaug.util import Action
+
+def augmentData(text, mode):
+    '''mode is either insert or substitute'''
+    aug = naw.WordEmbsAug(
+        model_type='word2vec', model_path=model_dir+'GoogleNews-vectors-negative300.bin',
+        action=mode)
+    augmented_text = aug.augment(text)
