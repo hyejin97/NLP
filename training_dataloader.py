8f140deeda1868d2991cbc13308e6d95188fa1c0
aish1795 <aishwarya.raman1795@gmail.com>
2021-05-08 17:05:06 -0400

    Update training_dataloader.py

--- training_dataloader.py
+++ training_dataloader.py
@@ -1,18 +1,18 @@
 from datasets import load_dataset, concatenate_datasets
 class TrainDataLoader:
-    def load_train_data():
-        return concatenate_datasets(load_about_data("train"), load_as_data("train"), load_to_data("train"))
+    def load_train_data(self):
+        return concatenate_datasets(self.load_about_data("train"), self.load_as_data("train"), self.load_to_data("train"))
 
-    def load_about_train_data():
-        return load_about_data("train")
+    def load_about_train_data(self):
+        return self.load_about_data("train")
 
-    def load_as_train_data():
-        return load_as_data("train")
+    def load_as_train_data(self):
+        return self.load_as_data("train")
 
-    def load_to_train_data():
-        return load_to_data("train")
+    def load_to_train_data(self):
+        return self.load_to_data("train")
 
-    def load_about_data(split):
+    def load_about_data(self, split):
         funpedia = load_dataset('md_gender_bias', 'funpedia', split=split)
         funpedia = funpedia.rename_column('gender', 'label')
 
@@ -22,7 +22,7 @@ class TrainDataLoader:
         wizard = wizard.rename_column('gender', 'label')
         return concatenate_datasets(wizard, funpedia)
 
-    def load_as_data(split):
+    def load_as_data(self, split):
         yelp = load_dataset('md_gender_bias', 'yelp_inferred', split=split)
         yelp = yelp.rename_column('ternary_label', 'label')
 
@@ -31,7 +31,7 @@ class TrainDataLoader:
 
         return concatenate_datasets(yelp, convai2)
 
-    def load_to_data(split):
+    def load_to_data(self, split):
         light = load_dataset('md_gender_bias', 'light_inferred', split=split)
         light = light.rename_column('ternary_label', 'label')
 
